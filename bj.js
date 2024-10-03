f0b0751ef1366a187a5679094b01a2d90619ac6e
srithwak <srithwak@gmail.com>
2024-10-03 08:31:40 -0400

    push from oct 2nd

--- bj.js
+++ bj.js
@@ -1,20 +1,22 @@
+const prompt = require('prompt-sync')();
+
 const faceValues = {
-   '2':2,
-   '3':3,
-   '4':4,
-   '5':5,
-   '6':6,
-   '7':7,
-   '8':8,
-   '9':9,
-   'T':10,
-   'J':10,
-   'Q':10,
-   'K':10,
-   'A':11,
+   '2': 2,
+   '3': 3,
+   '4': 4,
+   '5': 5,
+   '6': 6,
+   '7': 7,
+   '8': 8,
+   '9': 9,
+   'T': 10,
+   'J': 10,
+   'Q': 10,
+   'K': 10,
+   'A': 11,
 }
 
-const createDeck = (numDecks = 1, shuffle = true) => {
+const createDeck = (numDecks = 1, shuffle = false) => {
    let deck = [];
    for (let i = 0; i < numDecks; i++) {
       for (let j of 'SCDH'.split('')) {
@@ -39,17 +41,65 @@ const createPlayers = numPlayers => {
    let players = [];
    for (let i = 0; i < numPlayers; i++) {
       players.push({
-         playerID: i + 1,
          points: 0,
          hand: [],
-         state: 0, // 0 = playing, 1 = win, 2 = lose
          ace: false
       });
    }
    return players;
 }
 
-let deck = createDeck();
+// let deck = createDeck();
+[
+   { suit: 'S', value: 2 },
+   { suit: 'S', value: 3 },
+   { suit: 'S', value: 4 },
+   { suit: 'S', value: 5 },
+   { suit: 'S', value: 6 },
+   { suit: 'S', value: 7 },
+   { suit: 'S', value: 8 },
+   { suit: 'S', value: 9 },
+   { suit: 'S', value: 10 },
+   { suit: 'S', value: 10 },
+   { suit: 'S', value: 10 },
+   { suit: 'S', value: 10 },
+   { suit: 'S', value: 11 },
+   { suit: 'C', value: 2 },
+   { suit: 'C', value: 3 },
+   { suit: 'C', value: 4 },
+   { suit: 'C', value: 5 },
+   { suit: 'C', value: 6 },
+   { suit: 'C', value: 7 },
+   { suit: 'C', value: 8 },
+   { suit: 'C', value: 9 },
+   { suit: 'C', value: 10 },
+   { suit: 'C', value: 10 },
+   { suit: 'C', value: 10 },
+   { suit: 'C', value: 10 },
+   { suit: 'C', value: 11 },
+   { suit: 'D', value: 2 },
+   { suit: 'D', value: 3 },
+   { suit: 'D', value: 4 },
+   { suit: 'D', value: 5 },
+   { suit: 'D', value: 6 },
+   { suit: 'D', value: 7 },
+   { suit: 'D', value: 8 },
+   { suit: 'D', value: 9 },
+   { suit: 'D', value: 10 },
+   { suit: 'D', value: 10 },
+   { suit: 'D', value: 10 },
+   { suit: 'D', value: 10 },
+   { suit: 'D', value: 11 },
+   { suit: 'H', value: 2 },
+   { suit: 'H', value: 3 },
+   { suit: 'H', value: 4 },
+   { suit: 'H', value: 5 },
+   { suit: 'H', value: 6 },
+   { suit: 'H', value: 7 },
+   { suit: 'H', value: 8 },
+   { suit: 'H', value: 9 },
+   { suit: 'H', value: 10 }
+]
 let players = createPlayers(2);
 
 const checkAces = () => {
@@ -71,15 +121,45 @@ const dealHand = () => {
          checkAces();
       }
    }
+   for (let i = 0; i < players.length; i++) {
+      checkForWin(i);
+   }
 }
 
 dealHand();
 
-// checkAces();
+const hit = playerID => {
+   let card = deck.pop();
+   players[playerID].hand.push(card);
+   players[playerID].points += card.value;
+   if (checkForWin(playerID)) {
+      console.log(`Player ${playerID + 1} won.`);
+   }
+}
 
-// console.log(deck.length);
-console.log(deck);
-console.log(players[0]);
-console.log(players[0].hand);
-console.log(players[1]);
-console.log(players[1].hand);
+function checkForWin(id) {
+   if (players[id].points == 21) return true;
+}
+
+function playGame() {
+   for (let p = 0; p < players.length; p++) {
+      console.log(`Player ${p + 1} turn`);
+      let cardDisp = '';
+      for (let i = 0; i < players[p].hand.length; i++)
+         // color the input and change the letteer to icons
+         cardDisp += `${players[p].hand[i].suit}${players[p].hand[i].value} `;
+      console.log(cardDisp);
+      console.log(players[p].points);
+
+      let inp = prompt(('h/s: '));
+      if (inp.toLowerCase == 'h' || inp.toLowerCase == 'hit')
+         hit(p);
+      else if (inp.toLowerCase() == 's' || inp.toLowerCase == 'stand')
+         console.log(`Player ${p + 1} stood`);
+
+   }
+}
+
+playGame();
+
+console.log(deck)
--- bj2.js
+++ bj2.js
@@ -1,29 +1,17 @@
 const prompt = require('prompt-sync')();
 
-let hand = [];
-let player0 = { hand: [{ suite: 'p' }] };
-let player1 = { hand: [{ suite: 'q' }] };
-let turn = true;
-
-function getRandomInt(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
- }
-
 function playGame() {
-   if (turn) {
-      //code for player 1
-      for (let i = 0; i < player0.hand.length; i++)
-         console.log(`Card: Suite ${player0.hand[i].suite}, Number: ${player0.hand[i].value}`);
+   for (let p = 0; p < players.length; p++) {
+      console.log(`Player ${p+1} turn`);
+      for (let i = 0; i < players[i].hand.length; i++)
+         console.log(`Card: Suite ${players[i].hand[i].suite}, Number: ${players[i].hand[i].value}`);
       let inp = prompt(('h/s: '));
       if (inp == 'h') {
-         let rand = getRandomInt(0, hand.length);
-         player0.hand.push(hand[rand]);
-         deck.splice(rand, 1);
+         let card = deck.pop();
+         players[i].hand.push(card);
+         players[i].points += card.value;
       }
-   } else {
-      //code for player2
    }
-   turn = !turn;
 }
 
 playGame();
